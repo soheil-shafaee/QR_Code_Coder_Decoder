b55e202d0407f629982d6efcb0d45b890fe5cf07
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2025-01-07 23:52:11 +0330

    Connect between wellcome page and qr_code_scanner page

--- wellcome_page.py
+++ wellcome_page.py
@@ -1,17 +1,35 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 from PyQt5 import uic
+from qr_code_scanner import QRScanner
 import sys
 
 
 class WellcomePage(QMainWindow):
+    """This Class displays wellcome window. By using start button you can
+    start to use this app."""
+
     def __init__(self):
         super(WellcomePage, self).__init__()
 
         # Loading Wellcome Page UI
         uic.loadUi("wellcome_page.ui", self)
 
+        # Finding Button
+        self.start_button = self.findChild(QPushButton, "start_button")
+
+        # Using button
+        self.start_button.clicked.connect(self.let_start)
+
+        # Define Variable for using scanner page
+        self.scanner_page = None
+
         # Display Wellcome Page
         self.show()
+
+    def let_start(self):
+        self.scanner_page = QRScanner()
+        self.scanner_page.show()
+        self.close()
 
 
 if __name__ == "__main__":
