2afacf9e9ebf6aa797f5045b27bd47a68a55a75c
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2025-03-14 01:01:52 +0330

    Set Fixed size to window

--- choose_content_page.py
+++ choose_content_page.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
 import sys
 
@@ -8,14 +9,33 @@ class ChooseContent(QMainWindow):
         super().__init__()
         self.load_ui()
 
+
+        self.close_button = self.findChild(QPushButton, "close_button")
+        self.mini_button = self.findChild(QPushButton, "mini_button")
+
+
+
+
     def load_ui(self) -> None:
         try:
-            self.setFixedSize(423, 513)
             uic.loadUi("generate_first_page.ui", self)
+            self.setFixedSize(423, 513)
+            self.setWindowFlag(Qt.FramelessWindowHint)
+            self.close_button.clicked.connect(self.close_win)
+            self.mini_button.clicked.connect(self.mini_win)
 
         except Exception as e:
             print(f"Error: {e}")
             sys.exit(1)
+
+    def close_win(self):
+        try:
+            self.close()
+        except Exception as e:
+            print(e)
+
+    def mini_win(self):
+        self.showMinimized()
 
 
 def main() -> None:
