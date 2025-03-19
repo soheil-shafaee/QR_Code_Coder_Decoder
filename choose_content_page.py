abbe2486b7d7c595212482f6ffd39e64cc8916bd
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2025-03-20 00:11:00 +0330

    Connect with Email page

--- choose_content_page.py
+++ choose_content_page.py
@@ -5,6 +5,7 @@ import sys
 
 import qr_code_scanner as qr
 import qr_history as qr_history
+from email_page import EmailPage
 
 
 class ChooseContent(QMainWindow):
@@ -12,19 +13,34 @@ class ChooseContent(QMainWindow):
         super().__init__()
         self.load_ui()
 
-        self.scanning: qr.QRScanner | None = None
+        # Define Variables
+        self.show_scanning: qr.QRScanner | None = None
         self.show_history: qr_history.HistoryPage | None = None
-
+        self.show_email: EmailPage | None = None
 
         # Define Widgets
-        self.scan_page = self.findChild(QPushButton, "scan_button")
+        """Top Navbar"""
         self.close_button = self.findChild(QPushButton, "close_button")
         self.mini_button = self.findChild(QPushButton, "mini_button")
 
+        """Main Body"""
+        self.email_button = self.findChild(QPushButton, "email_button")
+
+        """Button Navbar"""
+        self.scan_page = self.findChild(QPushButton, "scan_button")
+        self.history_page = self.findChild(QPushButton, "history_button")
+
         # Do Action with our widgets
-        self.scan_page.clicked.connect(self.go_to_scan)
+        """Top Navbar"""
         self.close_button.clicked.connect(self.close_win)
         self.mini_button.clicked.connect(self.mini_win)
+
+        """Main Body"""
+        self.email_button.clicked.connect(self.go_to_email_page)
+
+        """Button Navbar"""
+        self.scan_page.clicked.connect(self.go_to_scan)
+        self.history_page.clicked.connect(self.go_to_history)
 
     def load_ui(self) -> None:
         try:
@@ -36,22 +52,38 @@ class ChooseContent(QMainWindow):
             print(f"Error: {e}")
             sys.exit(1)
 
-    def go_to_scan(self):
-        try:
-            self.scanning = qr.QRScanner()
-            self.scanning.show()
-            self.close()
-        except Exception as e:
-            print(e)
-
-    def close_win(self):
+    def close_win(self) -> None:
         try:
             self.close()
         except Exception as e:
             print(e)
 
-    def mini_win(self):
+    def mini_win(self) -> None:
         self.showMinimized()
+
+    def go_to_email_page(self) -> None:
+        try:
+            self.show_email = EmailPage()
+            self.show_email.show()
+            self.close()
+        except Exception as e:
+            print(e)
+
+    def go_to_scan(self) -> None:
+        try:
+            self.show_scanning = qr.QRScanner()
+            self.show_scanning.show()
+            self.close()
+        except Exception as e:
+            print(e)
+
+    def go_to_history(self) -> None:
+        try:
+            self.show_history = qr_history.HistoryPage()
+            self.show_history.show()
+            self.close()
+        except Exception as e:
+            print(e)
 
 
 def main() -> None:
