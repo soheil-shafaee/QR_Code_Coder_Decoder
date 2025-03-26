fe3c1cba70c91ef86a8332db487a2b510921e763
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2025-03-27 03:00:15 +0330

    Add message box for email and Making blur Background Pic

--- email_page.py
+++ email_page.py
@@ -1,15 +1,18 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QLineEdit, QMessageBox, QGraphicsBlurEffect
 from PyQt5.QtCore import Qt
 from PyQt5 import uic
 import sys
 
 from verify.email_verify import email_checking
 
+
 class EmailPage(QMainWindow):
     def __init__(self):
         super().__init__()
         self.load_ui()
 
+        # Variable
+        self.blur_effect: QGraphicsBlurEffect | None = None
 
         # Define Widgets
         """Navbar"""
@@ -19,6 +22,7 @@ class EmailPage(QMainWindow):
         """Main Body"""
         self.email_link = self.findChild(QLineEdit, "email_link")
         self.generate_button = self.findChild(QPushButton, "generate_button")
+        self.background_image = self.findChild(QLabel, "background_image")
         # Do Action with widgets
         """Navbar"""
         self.close_button.clicked.connect(self.close)
@@ -26,7 +30,6 @@ class EmailPage(QMainWindow):
 
         """Main Body"""
         self.generate_button.clicked.connect(self.generate_qr_code)
-
 
     def load_ui(self) -> None:
         try:
@@ -36,6 +39,11 @@ class EmailPage(QMainWindow):
             self.setFixedSize(423, 513)
             # Remove window navbar
             self.setWindowFlag(Qt.FramelessWindowHint)
+            # Making Background Picture blur
+            self.blur_effect = QGraphicsBlurEffect()
+            self.blur_effect.setBlurRadius(15)
+            self.background_image.setGraphicsEffect(self.blur_effect)
+
         except Exception as e:
             print(e)
             sys.exit(1)
@@ -44,12 +52,22 @@ class EmailPage(QMainWindow):
     def email_text_line(self):
         return self.email_link.text()
 
-
     def generate_qr_code(self):
         try:
-            print(self.email_text_line())
-        except Exception as e:
+            email_valid = self.email_text_line()
+            if not email_valid:
+                message = QMessageBox()
+                message.setWindowTitle("Value Error")
+                message.setIcon(QMessageBox.Warning)
+                message.setText("Email Not Valid")
+                message.exec_()
+                return
+            else:
+                print(email_valid)
+        except ValueError as e:
             print(e)
+
+
 def main():
     try:
         app = QApplication(sys.argv)
